bb7043cacefe61fcf64688b53d17b91bf753baae
mcgnly <katie@mcgnly.com>
2020-04-16 12:12:22 +0200

    start from placeholder

--- src/views/ScanBox.js
+++ src/views/ScanBox.js
@@ -1,36 +1,10 @@
-import React, { useState } from 'react';
-import QrReader from 'react-qr-reader';
-import {Link} from "react-router-dom";
+import React from 'react';
 
 function ScanBox({authObject}) {
-  const [data, setData] = useState('')
-
   return (
     <div>
       <h2>Scan a box now:</h2>
-      {data ? 
-      <div>
-        <a href={data}>{data}</a> 
-        <br />
-        <button 
-        onClick={()=>setData('')}
-        className="m-1 bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
-        >Scan again</button>
-      </div>
-      :
-      <QrReader
-          delay={300}
-          onError={(err) => console.log(err)}
-          onScan={setData}
-          style={{ width: '100%' }}
-        />}
-
-        <Link 
-        to='/'
-        className="m-1 leading-loose bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
-        >
-          Go Home
-        </Link>
+      <p>I'm a placeholder</p>
     </div>
   )
 }
